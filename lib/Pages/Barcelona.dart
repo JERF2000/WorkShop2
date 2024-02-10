20baf6b66011ebf33d0fefaaf660dbff2e0db168
Jasir Reyes <esaud278@gmail.com>
2024-02-10 12:06:14 -0600

    Se justifico el texto en ambas paginas

--- lib/Pages/Barcelona.dart
+++ lib/Pages/Barcelona.dart
@@ -22,17 +22,20 @@ class Barcelona extends StatelessWidget{
             ),
 
             Text("El Fútbol Club Barcelona (en catalán: Futbol Club Barcelona), conocido " + 
-            "popularmente como Barça,n. 1​ es una entidad polideportiva con sede en Barcelona, " + 
+            "popularmente como Barça, es una entidad polideportiva con sede en Barcelona, " + 
             "Cataluña, España. Fue fundado como club de fútbol el 29 de noviembre de 1899 y" + 
-            " registrado oficialmente el 5 de enero de 1903.\n"),
+            " registrado oficialmente el 5 de enero de 1903.\n",
+            textAlign: TextAlign.justify),
         
             Text("A nivel institucional es uno de los cuatro clubes profesionales de fútbol" + 
-            " del país cuya entidad jurídica no es la de sociedad anónima deportiva (S. A. D.),​" + 
-            " ya que su propiedad recae en sus más de 143 000 socios.\n"),
+            " del país cuya entidad jurídica no es la de sociedad anónima deportiva (S. A. D.)" + 
+            " ya que su propiedad recae en sus más de 143 000 socios.\n",
+            textAlign: TextAlign.justify),
         
             Text("Sumando torneos nacionales e internacionales, es el segundo club español " + 
-            "más laureado,16​ a nivel nacional domina el palmarés con setenta y siete campeonatos," + 
-            "y a nivel internacional ostenta veintidós trofeos, situado en el segundo puesto europeo.\n"),
+            "más laureado,16 a nivel nacional domina el palmarés con setenta y siete campeonatos," + 
+            "y a nivel internacional ostenta veintidós trofeos, situado en el segundo puesto europeo.\n",
+            textAlign: TextAlign.justify),
         
             SizedBox(height: 30.0,),
 
--- lib/Pages/RealMadrid.dart
+++ lib/Pages/RealMadrid.dart
@@ -21,20 +21,25 @@ class RealMadrid extends StatelessWidget{
               style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
             ),
 
-            Text("El Real Madrid es una entidad polideportiva con sede en Madrid, España.\n"),
+            Text("El Real Madrid es una entidad polideportiva con sede en Madrid, España.\n",
+            textAlign: TextAlign.justify,),
         
             Text("El Real Madrid fue fundado en 1902. Julián Palacios es el primer presidente, " + 
             "pero fue Juan Padrós, catalán y de Barcelona, quien formalmente constituyó la " + 
             "institución ese año. El interés en el fútbol crecía tanto en España que el Madrid " + 
             "propuso realizar un torneo en honor al rey Alfonso XIII. La iniciativa se convirtió " + 
-            "en la Copa de España (Copa del Rey).\n"),
+            "en la Copa de España (Copa del Rey).\n",
+            textAlign: TextAlign.justify,
+            ),
         
             Text("El Real Madrid es el rey de Europa y España y aunque los rivales han ido " + 
-            "y venido para enfrentarse a él en la historia del fútbol, nadie se acerca a su legado.\n"),
+            "y venido para enfrentarse a él en la historia del fútbol, nadie se acerca a su legado.\n",
+            textAlign: TextAlign.justify,),
         
             Text("El estadio Santiago Bernabéu tiene una capacidad de 81.000 espectadores. " + 
             "En torno a un 80% del aforo está ocupado por socios del Real Madrid que tienen un " + 
-            "abono, mientras que el resto de las entradas se venden al público general.\n"),
+            "abono, mientras que el resto de las entradas se venden al público general.\n",
+            textAlign: TextAlign.justify,),
         
             SizedBox(height: 30.0,),
 
